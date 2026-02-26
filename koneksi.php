f8b1c78be45e16dd8cbcce3588711669879141dc
Dhimas Raditya <dhimas.raditia2005@gmail.com>
2026-02-26 14:21:55 +0700

    Upload sistem CRUD hotel perdana

---
+++ koneksi.php
@@ -0,0 +1,13 @@
+<?php
+$host = '127.0.0.1';
+$dbname = 'db_hotel';
+$username = 'adminremote'; // User MariaDB Debian Anda
+$password = 'password_rahasia'; // Password MariaDB Debian Anda
+
+try {
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    die("Koneksi database gagal: " . $e->getMessage());
+}
+?>
